d2a688b15b08d1b0d22ce92fc15ab6d9df11e4f5
Askunia <alvarius75@hotmail.com>
2010-06-04 19:22:49 +0200

    Added options and Frame is now moveable and 10man handling

--- SLPlagueCounter.lua
+++ SLPlagueCounter.lua
@@ -5,16 +5,67 @@ local AceEvent = LibStub("AceEvent-3.0")
 -- Unbound Plague 10man ID 72855
 --                25man ID 72856
 
+SLPlagueCounter.options = {
+	name = "Slap Plague Counter",
+	handler = SLPlagueCounter,
+	type = "group",
+	get = "OptionsGet",
+	set = "OptionsSet",
+	args = {
+		locked = {
+			type = "toggle",
+			get = "GetLock",
+			set = "SetLock",
+			name = "Locked",
+			desc = "Locked",
+			order = 1,
+		},
+		width = {
+			type = "range",
+			min = 20,
+			max = 100,
+			step = 10,
+			name = "Width",
+			desc = "Icon Width",
+			order = 3,
+		},
+		height = {
+			type = "range",
+			min = 20,
+			max = 100,
+			step = 10,
+			name = "Height",
+			desc = "Icon Height",
+			order = 4,
+		},
+	}
+}
+
+local defaults = {
+	profile = {
+		xOffset = 0,
+		yOffset = 0,
+		width = 80,
+		height = 80,
+		locked = true,
+	}
+}
+
 function SLPlagueCounter:OnEnable()
 	self.TimeSinceLastUpdate=0
 	self.updateInterval=1.0
 	self.enabled=false
 	self:UnregisterAllEvents()
 	self:RegisterEvent("ZONE_CHANGED_INDOORS","ZoneChanged")
+	SLPlagueCounter:CreateAnchorFrame()
 end
 
 function SLPlagueCounter:OnInitialize()
 	self:RegisterChatCommand("slplague", "Command")
+	self.db = LibStub("AceDB-3.0"):New("SLPlagueCounterDB", defaults, "Default")
+	self.options.args.profiles = LibStub("AceDBOptions-3.0"):GetOptionsTable(self.db)
+	LibStub("AceConfig-3.0"):RegisterOptionsTable("SLPlagueCounter", self.options)
+	self.optionsFrame = LibStub("AceConfigDialog-3.0"):AddToBlizOptions("SLPlagueCounter", "SLPlagueCounter")
 end
 
 function SLPlagueCounter:Command()
@@ -36,6 +87,40 @@ function SLPlagueCounter:ZoneChanged()
 	end
 end
 
+function SLPlagueCounter:CreateAnchorFrame()
+	local anchorFrame = CreateFrame("Frame", "AnchorFrame", UIParent)
+	self.anchorFrame = anchorFrame
+	anchorFrame:SetWidth(self.db.profile.width)
+	anchorFrame:SetHeight(self.db.profile.height)
+	anchorFrame:SetPoint("CENTER",UIParent,"CENTER",self.db.profile.xOffset,self.db.profile.yOffset)
+	anchorFrame:SetMovable(true)
+	anchorFrame:EnableMouse(true)
+	anchorFrame:RegisterForDrag("LeftButton")
+	anchorFrame:SetScript("OnDragStart", function(this)
+		anchorFrame.oldxOffset = this:GetLeft()
+		anchorFrame.oldyOffset = this:GetTop()
+		this:StartMoving()
+	end)
+	anchorFrame:SetScript("OnDragStop", function(this)
+		this:StopMovingOrSizing()
+		SLPlagueCounter:SaveAnchorPos(anchorFrame, this:GetLeft(), this:GetTop())
+	end)
+	if (self.db.profile.locked) then
+		anchorFrame:Hide()
+	else
+		anchorFrame:Show()
+	end
+	local background = anchorFrame:CreateTexture("AnchorFrameBG", "BACKGROUND")
+	background:SetTexture("Interface\\Icons\\Spell_Shadow_CorpseExplode")
+	background:SetAllPoints()
+	print("Anchor Frame created")
+end
+
+function SLPlagueCounter:SaveAnchorPos(anchorFrame, xOffset, yOffset)
+	self.db.profile.xOffset = self.db.profile.xOffset + xOffset - anchorFrame.oldxOffset 
+	self.db.profile.yOffset = self.db.profile.yOffset + yOffset - anchorFrame.oldyOffset
+end
+
 function SLPlagueCounter:SpellApplied(event, timestamp, eventType, srcGuid, srcName, srcFlags, dstGuid, dstName, dstFlags, ... )
 	-- Something got applied, check if it was on us !
 	if (dstName==UnitName("player")) then
@@ -53,12 +138,24 @@ function SLPlagueCounter:SpellApplied(event, timestamp, eventType, srcGuid, srcN
 				-- Create the Frame :)
 				self:CreatePlagueFrame(maxTime)
 			end
+			if (spellID=72855) then -- 10man Heroic Version
+				-- Check if we already have sickness debuff
+				local name, _, _, count = UnitDebuff("player","Plague Sickness")
+				local maxTime = 0
+				if not name then
+					maxTime = 16
+				else
+					maxTime = 16/(count+1)
+				end
+				-- Create the Frame :)
+				self:CreatePlagueFrame(maxTime)
+			end
 		end
 	end
 end
 
 function SLPlagueCounter:CreatePlagueFrame(maxTime)
-	local timerFrame = CreateFrame("FRAME","PlagueFrame",UIParent)
+	local timerFrame = CreateFrame("FRAME","PlagueFrame",self.anchorFrame)
 	local background = timerFrame:CreateTexture("PlagueFrameBG", "BACKGROUND")
 	local tfontString = timerFrame:CreateFontString("$parentText", "ARTWORK", "GameFontNormal")
 	
@@ -67,9 +164,9 @@ function SLPlagueCounter:CreatePlagueFrame(maxTime)
 
 	timerFrame.value = maxTime
 	timerFrame.elapsedTime = 0
-	timerFrame:SetWidth(80)
-	timerFrame:SetHeight(80)
-	timerFrame:SetPoint("CENTER",UIParent,0,200)
+	timerFrame:SetWidth(self.db.profile.width)
+	timerFrame:SetHeight(self.db.profile.height)
+	timerFrame:SetPoint("CENTER",UIParent,"CENTER",self.db.profile.xOffset,self.db.profile.yOffset)
 	
 	tfontString:SetAllPoints()
 	tfontString:SetFont("Fonts\\FRIZQT__.TTF",40,"MONOCHROME")
@@ -96,5 +193,26 @@ function SLPlagueCounter:UpdatePlagueFrame(timerFrame, elapsed)
 	-- Change Color of Text to Red 2 Seconds before we have to pass it off
 	if ((timerFrame.value-timerFrame.elapsedTime)<=2) then
 		timerFrame.tfontString:SetTextColor(1,0,0,1)
+	end
+end
+
+function SLPlagueCounter:OptionsGet(info)
+	return self.db.profile[info[#info]]
+end
+
+function SLPlagueCounter:OptionsSet(info,value)
+	self.db.profile[info[#info]] = value
+end
+
+function SLPlagueCounter:GetLock(info)
+	return self.db.profile.locked
+end
+
+function SLPlagueCounter:SetLock(info,value)
+	self.db.profile.locked = value
+	if (value) then
+		self.anchorFrame:Hide()
+	else
+		self.anchorFrame:Show()
 	end
 end
